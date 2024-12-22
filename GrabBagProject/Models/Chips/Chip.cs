221a05dec36f0ca318282a2c5eaff5471bbe3efa
charder <chardergaming@gmail.com>
2024-12-22 00:12:39 -0500

    Bringing back RAM logic from previous prototype.

---
+++ GrabBagProject/Models/Chips/Chip.cs
@@ -0,0 +1,21 @@
+﻿using GrabBagProject.Models.Items;
+using GrabBagProject.Models.Modifiers;
+
+namespace GrabBagProject.Models.Chips
+{
+    internal class Chip : Item
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Value { get; set; }
+        public Chip() { }
+
+        public Chip Build(string name, string description, int value = 0)
+        {
+            Name = name;
+            Description = description;
+            Value = value;
+            return this;
+        }
+    }
+}
